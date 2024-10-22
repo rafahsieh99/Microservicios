9260db68469985d9e3591dcb38925d5f438f271c
rafahsieh99 <rafahsieh99@gmail.com>
2024-10-22 19:19:38 -0300

    Actualizacion de db

--- productos/db.py
+++ productos/db.py
@@ -1,5 +1,5 @@
 import psycopg2
-from config import username_productos_db, password_productos_db
+from inventario.config import username_productos_db, password_productos_db
 
 def get_db_connection():
     conn = psycopg2.connect(
